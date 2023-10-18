2c7eaed973259fb6315a00b0315aa8e49f53a12c
brandnholl <brandnlholl@gmail.com>
2023-10-17 23:21:16 -0700

    Left and Right half pyramids

---
+++ StarProject.java
@@ -0,0 +1,8 @@
+public class StarProject {
+    public static void main(String[] args) {
+        Stars star = new Stars();
+        star.rightHalfPyramid();
+        star.leftHalfPyramid();
+        star.fullPyramid();
+    }
+}
---
+++ Stars.java
@@ -0,0 +1,22 @@
+public class Stars {
+    public void rightHalfPyramid() {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public void leftHalfPyramid(){
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= 5 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
