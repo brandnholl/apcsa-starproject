f3ec7aa472fab77f694d109894fff3edb6796d11
brandnholl <brandnlholl@gmail.com>
2023-10-18 23:49:18 -0700

    Formatting and added methods for all shapes

--- StarProject.java
+++ StarProject.java
@@ -7,5 +7,13 @@ public class StarProject {
         star.invertedRightHalfPyramid();
         star.invertedLeftHalfPyramid();
         star.invertedFullPyramid();
+        star.rhombusPattern();
+        star.diamondPattern();
+        star.hourglassPattern();
+        star.hollowSquarePattern();
+        star.hollowFullPyramid();
+        star.hollowInvertedFullPyramid();
+        star.hollowDiamondPyramid();
+        star.floydsTriangle();
     }
 }
--- Stars.java
+++ Stars.java
@@ -9,7 +9,7 @@ public class Stars {
         System.out.println();
     }
 
-    public void leftHalfPyramid(){
+    public void leftHalfPyramid() {
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= 5 - i; j++) {
                 System.out.print("  ");
@@ -22,7 +22,7 @@ public class Stars {
         System.out.println();
     }
 
-    public void fullPyramid(){
+    public void fullPyramid() {
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j <= 5 - i; j++) {
                 System.out.print(" ");
@@ -35,7 +35,7 @@ public class Stars {
         System.out.println();
     }
 
-    public void invertedRightHalfPyramid(){
+    public void invertedRightHalfPyramid() {
         for (int i = 1; i <= 6; i++) {
             for (int j = 1; j <= 6 - i; j++) {
                 System.out.print(" *");
@@ -47,7 +47,7 @@ public class Stars {
         }
     }
 
-    public void invertedLeftHalfPyramid(){
+    public void invertedLeftHalfPyramid() {
         for (int i = 1; i <= 5; i++) {
             for (int j = 1; j < i; j++) {
                 System.out.print("  ");
@@ -60,7 +60,7 @@ public class Stars {
         System.out.println();
     }
 
-    public void invertedFullPyramid(){
+    public void invertedFullPyramid() {
         for (int i = 5; i >= 1; i--) {
             for (int j = 1; j <= 5 - i; j++) {
                 System.out.print(" ");
@@ -70,6 +70,64 @@ public class Stars {
             }
             System.out.println();
         }
+        System.out.println();
+    }
 
+    public void rhombusPattern() {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j < 5; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public void diamondPattern() {
+        System.out.println();
+    }
+
+    public void hourglassPattern() {
+
+        System.out.println();
+    }
+
+    public void hollowSquarePattern() {
+        for (int i = 1; i <= 6; i++){
+            if(i == 1 || i ==6){
+                System.out.println("* * * * * *");
+            }
+            else{
+                System.out.println("*         *");
+            }
+        }
+        System.out.println();
+    }
+
+    public void hollowFullPyramid() {
+        System.out.println();
+    }
+
+    public void hollowInvertedFullPyramid() {
+        System.out.println();
+    }
+
+    public void hollowDiamondPyramid() {
+        System.out.println();
+    }
+
+    public void hollowHourglassPattern() {
+        System.out.println();
+    }
+
+    public void floydsTriangle() {
+        System.out.println();
+    }
+
+    public void pascalsTriangle() {
+        System.out.println();
     }
 }
