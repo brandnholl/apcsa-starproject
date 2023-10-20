669fe4af29116f987cdeec48b89150877549cb0d
Brandon <brandnlholl@gmail.com>
2023-10-19 17:44:03 -0700

    Pascals Triangle

--- Stars.java
+++ Stars.java
@@ -206,8 +206,23 @@ public class Stars {
         System.out.println();
     }
 
+    public int factorial(int i) {
+        if (i == 0) {
+            return 1;
+        }
+        return i * factorial(i - 1);
+    }
     public void pascalsTriangle() {
-
+        for (int i = 0; i < (3 + 1); i++) {
+            for (int u = 0; u < (3 - (i)); u++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < i; k++) {
+                System.out.print(" " + factorial(i) / (factorial(i-k) * factorial(k)));
+            }
+            System.out.print(" 1");
+            System.out.println();
+        }
         System.out.println();
     }
 }
